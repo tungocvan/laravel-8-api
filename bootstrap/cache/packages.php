945286a87894b80df89dec94dad3dfa9b187f1fb
Tu Ngoc Van <tungocvan@gmail.com>
2023-04-15 11:49:16 +0700

    env

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,126 @@
+<?php return array (
+  'codexshaper/laravel-woocommerce' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Codexshaper\\WooCommerce\\WooCommerceServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Attribute' => 'Codexshaper\\WooCommerce\\Models\\Attribute',
+      'Category' => 'Codexshaper\\WooCommerce\\Models\\Category',
+      'Coupon' => 'Codexshaper\\WooCommerce\\Models\\Coupon',
+      'Customer' => 'Codexshaper\\WooCommerce\\Models\\Customer',
+      'Note' => 'Codexshaper\\WooCommerce\\Models\\Note',
+      'Order' => 'Codexshaper\\WooCommerce\\Models\\Order',
+      'PaymentGateway' => 'Codexshaper\\WooCommerce\\Facades\\PaymentGateway',
+      'Product' => 'Codexshaper\\WooCommerce\\Models\\Product',
+      'Refund' => 'Codexshaper\\WooCommerce\\Models\\Refund',
+      'Report' => 'Codexshaper\\WooCommerce\\Models\\Report',
+      'Review' => 'Codexshaper\\WooCommerce\\Models\\Review',
+      'Setting' => 'Codexshaper\\WooCommerce\\Models\\Setting',
+      'ShippingMethod' => 'Codexshaper\\WooCommerce\\Models\\ShippingMethod',
+      'ShippingZone' => 'Codexshaper\\WooCommerce\\Models\\ShippingZone',
+      'ShippingZoneMethod' => 'Codexshaper\\WooCommerce\\Models\\ShippingZoneMethod',
+      'System' => 'Codexshaper\\WooCommerce\\Models\\System',
+      'Tag' => 'Codexshaper\\WooCommerce\\Models\\Tag',
+      'Tax' => 'Codexshaper\\WooCommerce\\Models\\Tax',
+      'TaxClass' => 'Codexshaper\\WooCommerce\\Models\\TaxClass',
+      'Term' => 'Codexshaper\\WooCommerce\\Models\\Term',
+      'Variation' => 'Codexshaper\\WooCommerce\\Models\\Variation',
+      'Webhook' => 'Codexshaper\\WooCommerce\\Facades\\Webhook',
+      'WooCommerce' => 'Codexshaper\\WooCommerce\\Facades\\WooCommerce',
+      'WooAnalytics' => 'Codexshaper\\WooCommerce\\Facades\\WooAnalytics',
+      'Query' => 'Codexshaper\\WooCommerce\\Facades\\Query',
+    ),
+  ),
+  'facade/ignition' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Facade\\Ignition\\IgnitionServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Flare' => 'Facade\\Ignition\\Facades\\Flare',
+    ),
+  ),
+  'fruitcake/laravel-cors' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fruitcake\\Cors\\CorsServiceProvider',
+    ),
+  ),
+  'jgrossi/corcel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Corcel\\Laravel\\CorcelServiceProvider',
+    ),
+  ),
+  'laravel/sail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sail\\SailServiceProvider',
+    ),
+  ),
+  'laravel/sanctum' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sanctum\\SanctumServiceProvider',
+    ),
+  ),
+  'laravel/socialite' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Socialite\\SocialiteServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Socialite' => 'Laravel\\Socialite\\Facades\\Socialite',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'laravel/ui' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Ui\\UiServiceProvider',
+    ),
+  ),
+  'maatwebsite/excel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Maatwebsite\\Excel\\ExcelServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Excel' => 'Maatwebsite\\Excel\\Facades\\Excel',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+);
