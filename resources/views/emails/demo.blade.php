ac7b5e0516aca82f019f67ba89857de38d9cc1db
tungocvan <tungocvan@gmail.com>
2023-04-25 14:59:00 +0700

    xuly-3

--- resources/views/emails/demo.blade.php
+++ resources/views/emails/demo.blade.php
@@ -4,8 +4,8 @@
     <title>{{ $details['title'] }}</title>
 </head>
 <body>
-    <h1>{{ $details['title'] }}</h1>
-    <p>{{{ $details['body'] }}}</p>
+    <h1>{{ $details['title'] }}</h1>    
+    <p>{!! htmlspecialchars($details['body']) !!}</p>
     @if($details['attach'] !== null )
         <p><a href={{ $details['attach'] }}>Download file</a></p>    
     @endif
